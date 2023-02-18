64c7f73dc263468ee77cf036d47eed2acebe8179
Niku <nokka200@gmail.com>
2023-02-18 12:48:55 +0200

    Pari puuttui, lisätty

--- LogicTest/UnitTest1.cs
+++ LogicTest/UnitTest1.cs
@@ -4,10 +4,30 @@ namespace LogicTest
 {
     public class FirstTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 100)]
+        [InlineData(99, 100)]
+        [InlineData(55, 100)]
+        [InlineData(25, 100)]
+        public void CheckIfOverFlow_LessThanMax_ReturnTrue(int value, int max)
         {
-            var re = ValueChecker.Thingy();
+            // Act
+            var re = ValueChecker.CheckIfOverFlow(value, max);
+
+            // Assert
+            Assert.Equal(0, re);
         }
+
+        [Theory]
+        [InlineData(125, 100)]
+        public void CheckIfOverFlow_GreaterThanMax_ReturnTrue(int value, int max)
+        {
+            // Act
+            var re = ValueChecker.CheckIfOverFlow(value, max);
+
+            // Assert
+            Assert.Equal(25, re);
+        }
+
     }
 }
--- ValueTester/ValueChecker.cs
+++ ValueTester/ValueChecker.cs
@@ -2,9 +2,36 @@
 {
     static public class ValueChecker
     {
-        static public int Thingy()
+        /// <summary>
+        /// Checks if value is greater than maximun target value
+        /// </summary>
+        /// <param name="value">Value to check</param>
+        /// <param name="max">Compare value to this</param>
+        /// <returns>Difference, if value is less returns 0</returns>
+        static public int CheckIfOverFlow(int value, int max)
         {
-            return 1;
+            int re = 0;
+
+            if (value > max)
+            {
+                re = value - max;
+                return re;
+            }
+            else
+            {
+                return re;
+            }
+        }
+
+        /// <summary>
+        /// Checks if values are equal
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        static public bool CheckIfEven(int first, int second)
+        {
+            return (first == second);
         }
     }
 }
